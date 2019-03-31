c95709f9e64bd4692dc59ee381a121f71a43360d
Jenny Chu <jchudesign0930@gmail.com>
2019-03-31 16:36:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,4 @@ class Song
   
   attr_accessor :title, :artist
   
-  def initialize(title)
-    @title = title
-  
-  end
-end
-hotline_bling = Song.new("Hotline Bling")
-hotline_bling.title
-
+ end
