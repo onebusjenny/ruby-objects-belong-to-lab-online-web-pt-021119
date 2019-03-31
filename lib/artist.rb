752d230e89b2fd5e01238fb7bff6c23bc2149f86
Jenny Chu <jchudesign0930@gmail.com>
2019-03-31 16:28:29 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,11 @@
+class Artist
+  attr_accessor :name, :genre
+ 
+  def initialize(name, genre) 
+    @name = name
+    @genre = genre
+  end
+ 
+end
+ artist = Artist.new
+    artist.name = "Beyonce" 
